008edb25a7d0309f9e74ee0dd167ce5d24b21524
glanzmichael25 <glanz.michael.25@gmail.com>
2018-06-11 17:50:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/glanz/widgetapplication/ExampleAppWidgetProvider.java
@@ -0,0 +1,25 @@
+package com.example.glanz.widgetapplication;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        //loop through appWidgets
+        for(int appWidgetId : appWidgetIds){
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            //create object of remote views
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_widget);
+            views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent);
+            //since we are looping through appWidgets ids this call each appWidget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
